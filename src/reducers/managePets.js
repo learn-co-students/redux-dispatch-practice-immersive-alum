9cc0af17427eb2bbdd512d3814d6e2913d0e351e
ethanryan <ethanryan@gmail.com>
2018-09-12 20:24:06 -0400

    managePets reducer has cases for 'ADD_PET' and 'REMOVE_PET', and has a default state for pets

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,8 +1,20 @@
 export let state;
 
 
-export function managePets(state, action){
+export function managePets(state = {pets: []}, action){
   switch(action.type) {
+    case 'ADD_PET':
+    return {
+      pets: [
+        ...state.pets, action.pet
+      ]
+    }
+    case 'REMOVE_PET':
+    return {
+      pets: [
+        ...state.pets.filter(pet => pet.id !== action.id)
+      ]
+    }
     default:
     return state
   }
