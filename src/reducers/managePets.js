950f9432b7b20adfe76d773d6adf1565492483bd
Peter Sampson <peter.sampson010@gmail.com>
2020-04-09 09:29:06 +0100

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,25 @@
 export let state;
 
 
-export function managePets(){
+export function managePets(state={pets: []}, action){
+    if (action.type == "ADD_PET") {
+        return {pets: [...state.pets, action.pet]}
+    } else if (action.type == "REMOVE_PET") {
+        return {pets: state.pets.filter(x => x.id != action.id)}
+    } else {
+        return state
+    }
 }
 
-export function dispatch(){
+export function dispatch(action){
+    state = managePets(state, action)
+    render()
 }
 
 export function render(){
-
+    let container = documentt.getElementById('container');
+    let petsList = state.pets.map(pet => {
+        return <li>${pet.name}</li>
+    }).join(' ');
+    container.innerHTML  = <ul>${petsList}</ul>
 }
